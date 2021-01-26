6a2b4610b035efa83e06cfd1e004f5eb8bcd3843
XXX Ahana <ahana@XXXs-Air.hsd1.ca.comcast.net>
2021-01-25 17:34:46 -0800

    Display reply field on click and improve comment layout

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,15 @@
 module CommentsHelper
 end
+
+def short_age_string(time)
+  diff = Time.now - time # value is seconds (float)
+  diff = diff.abs.to_i
+  if diff >= 604_800 # seconds in a week
+    weeks = diff / 604_800
+    "#{weeks}#{'+' if weeks >= 999}w"
+  elsif diff > 3600 # seconds in an hour
+    "#{diff / 3600}h"
+  else
+    "#{diff / 60}m"
+  end
+end
