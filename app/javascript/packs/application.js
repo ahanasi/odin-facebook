7e250040e1422b268df383266a401a44de011d79
XXX Ahana <ahanasi@seas.upenn.edu>
2021-02-14 00:29:54 -0800

    Add user about info to Edit Profile layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@fortawesome/fontawesome-free/css/all"
 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
