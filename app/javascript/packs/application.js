4679b206d30e08547d6541a1cc404174a886be82
XXX Ahana <ahanasi@seas.upenn.edu>
2021-02-21 23:00:50 -0800

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "@fortawesome/fontawesome-free/js/all";
-import "@fortawesome/fontawesome-free/css/all";
-
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, faTwitter)
+// Kicks off the process of finding <i> tags and replacing with <svg>
+dom.watch()
 
 
 Rails.start()
